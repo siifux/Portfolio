e75fdf14ed3e3d479652617c34b611602861313e
Sindre <sindrefuru@hotmail.no>
2021-10-18 16:51:11 +0200

    Added components and Tailwind Css npm package

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 import "./App.css";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Testimonials from "./components/Testimonials";
 
 function App() {
-  
   return (
     <>
-      
+      <main>
+        <Navbar />
+        <About />
+        <Projects />
+        <Skills />
+        <Testimonials />
+        <Contact />
+      </main>
     </>
   );
 }
