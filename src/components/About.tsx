699350de08e675c9e4b012378fa7ff73b95d21fa
Sindre <sindrefuru@hotmail.no>
2021-10-18 19:19:20 +0200

    Added profileImg

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,26 +1,26 @@
 import React from "react";
 import Typewriter from "typewriter-effect";
+import profileName from '../assets/profile_2.jpg';
 
 export default function About() {
   return (
     <section id="about">
       <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+          <h1 className="title-font sm:text-4xl text-5xl mb-10 font-medium text-white">
             <Typewriter
-              options={{ loop: true }}
+              options={{ loop: true, delay: 50}}
               onInit={(typewriter) => {
                 typewriter
-                  .typeString("Hi, I'm Sindre!")
+                  .typeString("Hi, I'm Sindre! ✌️")
                   .pauseFor(2000)
                   .deleteAll()
                   .pauseFor(1000)
                   .start();
               }}
             />
-            <br className="hidden lg:inline-block" />
-            Passionately Cursious Developer, Driven By My Creative Mind.
           </h1>
+          <h1 className="title-font sm:text-2xl text-sm mb-2 font-medium text-white">Passionatly curious developer, driven by my creative mind!</h1>
           <p className="mb-8 leading-relaxed">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
             laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
@@ -45,7 +45,7 @@ export default function About() {
           <img
             className="object-cover object-center rounded"
             alt="hero"
-            src="./coding.svg"
+            src={profileName}
           />
         </div>
       </div>
